2e3ef040a07e6e175da00baa539807b793cd1cfb
Garvin Triskie <138429396+GarvinTris@users.noreply.github.com>
2025-11-18 15:05:16 +0700

    Add Jenis_Barang class with properties and methods

--- Fitur/Jenis_Barang.java
+++ Fitur/Jenis_Barang.java
@@ -1,1 +1,48 @@
+public class Jenis_Barang {
+    public int id_jenis_barang;
+    public Kelompok_Aset id_kelompok_aset;
+    public String nama_jenis_barang;
+    public string keterangan;
+
+    public Jenis_Barang(int id_jenis_barang, Kelompok_Aset id_kelompok_aset, String nama_jenis_barang,
+            String keterangan) {
+        this.id_jenis_barang = id_jenis_barang;
+        this.id_kelompok_aset = id_kelompok_aset;
+        this.nama_jenis_barang = nama_jenis_barang;
+        this.keterangan = keterangan;
+    }
+
+    public int getId_jenis_barang() {
+        return id_jenis_barang;
+    }
+
+    public void setId_jenis_barang(int id_jenis_barang) {
+        this.id_jenis_barang = id_jenis_barang;
+    }
+
+    public Kelompok_Aset getId_kelompok_aset() {
+        return id_kelompok_aset;
+    }
+
+    public void setId_kelompok_aset(Kelompok_Aset id_kelompok_aset) {
+        this.id_kelompok_aset = id_kelompok_aset;
+    }
+
+    public String getNama_jenis_barang() {
+        return nama_jenis_barang;
+    }
+
+    public void setNama_jenis_barang(String nama_jenis_barang) {
+        this.nama_jenis_barang = nama_jenis_barang;
+    }
+
+    public string getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(string keterangan) {
+        this.keterangan = keterangan;
+    }
+
+}
 
